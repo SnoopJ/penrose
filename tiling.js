b8ae295d663322db1e0e2a5b8f16e36d301af916
James <snoopjedi@gmail.com>
2015-10-22 01:02:48 -0500

    example of a by-hand assembly of an inflated thin rhomb

--- tiling.js
+++ tiling.js
@@ -38,14 +38,14 @@ drawnObj.prototype = {
     translate: function(x,y) {
         this.x += x
         this.y += y
-        this.matrix = this.matrix.translate( x, y )
+        this.matrix = I.translate( x, y ).multiply(this.matrix)
         this.draw()
     },
     setPosition: function(x,y) {
-        this.matrix = this.matrix.translate( -this.x, -this.y )
+        this.matrix = I.translate( -this.x, -this.y ).multiply(this.matrix)
         this.x = x
         this.y = y
-        this.matrix = this.matrix.translate( this.x, this.y )
+        this.matrix = I.translate( this.x, this.y ).multiply(this.matrix)
         this.draw()
     },
     setRotation: function(ang) {
@@ -122,7 +122,7 @@ function drawTriSides(tr) {
 // classes to contain the isosceles tris described in "Updown generation of Penrose patterns"
 // http://www.sciencedirect.com/science/article/pii/0019357790900058
 // 
-// local origin is the center of the defining cut
+// BEFORE any transformation, the center of the cut is at the local origin
 tri = function() {
     poly.apply(this)
     this.points = [
@@ -153,6 +153,8 @@ triA = function() {
         three: {pt1: this.points[2], pt2: this.points[0]},
         four: null
     }
+    this.draw()
+    d3.select(this.node.parentNode).append("text").text("A").attr("fill","white").attr("transform","translate("+side/4+",0)")
 }
 triA.prototype = $.extend({},tri.prototype,{
     alpha : 36, 
@@ -168,6 +170,8 @@ triAp = function() {
         three: {pt1: this.points[2], pt2: this.points[0]},
         four: null
     }
+    this.draw()
+    d3.select(this.node.parentNode).append("text").text("A'").attr("fill","white").attr("transform","translate(-"+side/3+",0)")
 }
 triAp.prototype = $.extend({},trip.prototype,{
     alpha : 36, 
@@ -175,14 +179,22 @@ triAp.prototype = $.extend({},trip.prototype,{
     color: "#FF00FF"    
 })
 
-triB = function() { tri.apply(this) }
+triB = function() { 
+    tri.apply(this) 
+    this.draw()
+    d3.select(this.node.parentNode).append("text").text("B").attr("fill","white").attr("transform","translate("+side/4+",0)")
+}
 triB.prototype = $.extend({},tri.prototype,{
     alpha : 108, 
     beta : 72/2, 
     color: "#0000FF"    
 })
 
-triBp = function() { trip.apply(this) }
+triBp = function() { 
+    trip.apply(this) 
+    this.draw()
+    d3.select(this.node.parentNode).append("text").text("B'").attr("fill","white").attr("transform","translate(-"+side/3+",0)")
+}
 triBp.prototype = $.extend({},trip.prototype,{
     alpha : 108, 
     beta : 72/2, 
@@ -236,15 +248,30 @@ thinrhomb.prototype = $.extend({},rhomb.prototype,{
 //thick.setPosition([-40,120])
 
 
-mytri = new triA(); 
-mytri.draw()
+mytri = new triA() 
 mytri.setRotation(0)
 mytri.setPosition(100,100)
 drawTriSides(mytri)
 
-mytri2 = new triAp();
-mytri2.draw()
-mytri2.setPosition(100,100)
+mytri2 = new triAp()
+mytri2.setPosition( 100 - 2*mytri2.points[1][0], 100 )
+mytri2.rotateAroundLocalPoint( mytri2.points[1], 180+mytri2.alpha ) // match up edges
 drawTriSides(mytri2)
+
+mytri3 = new triBp()
+mytri3.setPosition( 100 - mytri3.points[1][0], 100 + mytri3.points[1][1] + mytri.points[0][1])
+mytri3.rotateAroundLocalPoint( mytri3.points[1], mytri3.beta*1 ) // match up edges
+drawTriSides(mytri3)
+
+mytri4 = new triB()
+mytri4.setPosition(100 + mytri.points[1][0],100 - mytri4.points[2][1])
+mytri4.rotateAroundLocalPoint( mytri4.points[2], 180 ) // match up edges
+drawTriSides(mytri4)
+
+masterg = d3.select("svg").append("g").attr("transform","translate(200,00) rotate(45) ").attr("id","masterg").node()
+masterg.appendChild(mytri.node.parentNode)
+masterg.appendChild(mytri2.node.parentNode)
+masterg.appendChild(mytri3.node.parentNode)
+masterg.appendChild(mytri4.node.parentNode)
 
 d3.selectAll("polygon").attr("fill","black")
